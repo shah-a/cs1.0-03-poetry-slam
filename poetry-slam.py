72f7fc6fe32e6c52eb3582b1edabd0089ceebf67
Ali Shah <s.ashah@outlook.com>
2020-09-29 07:40:09 -0400

    added lines_printed_random() and equivalence_cycles()

--- poetry-slam.py
+++ poetry-slam.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def get_file_lines(filename):
 
     with open(filename, "r") as poem_lines:
@@ -11,7 +13,7 @@ def get_file_lines(filename):
 def lines_printed_backwards(lines_list):
 
     for _ in range(len(lines_list)):
-        lines_list[_] = f"{_ + 1} {lines_list[_]}"
+        lines_list[_] = f"{_ + 1} {lines_list[_]}" #adds line number to beginning of each string in list
 
     lines_list.reverse()
 
@@ -20,6 +22,7 @@ def lines_printed_backwards(lines_list):
 
     #  for _ in range(len(lines_list)):
     #     print(lines_list[::-1][_])
+    #
     # #Alternate solution to print the list in reverse order
     # #Previous solution uses list reverse() method, which reassigns to the variable
     # #This solution uses slicing with a negative step; it does not reassign any values to the list
@@ -27,5 +30,39 @@ def lines_printed_backwards(lines_list):
 
     return None
 
-lines_list = get_file_lines("tanka.txt")
-lines_printed_backwards(lines_list)
+def lines_printed_random(lines_list):
+
+    for _ in range(len(lines_list)):
+        i = len(lines_list) - 1
+        print(
+            lines_list[randint(0, i)]
+        )
+
+    return None
+
+# def equivalence_cycles(lines_list):
+
+#     new_list = []
+#     counter = 0
+#     while True:
+#         new_list.clear()
+#         counter += 1
+#         for _ in range(len(lines_list)):
+#             new_list.append(lines_list[randint(0, len(lines_list) - 1)])
+
+#         #print(new_list) #uncomment to print each list variation per cycle
+#         #input()         #uncomment to receive keyboard prompt to step through cycles; not recommended for lists with more than 3 elements
+
+#         if new_list[1:] == new_list[:-1]:
+#             break
+
+#     print(f"cycles: {counter}")
+
+#     # Soo... I got curious and wrote this function to count how many cycles it takes for every element to randomly be assigned the same value
+#     # It took 21530 cycles the first time I tried this with the poem from tanka.txt. Second attempt was around 64000, third was around 96000
+#     # All three attempts, each element ended up being 'A faint clap of thunder,' which makes sense because the poem has that line twice (lines 1 and 5)
+#     # To maximize likelihood for most number of cycles, each list element should be unique
+
+poem_lines = get_file_lines("tanka.txt")
+#lines_printed_backwards(poem_lines)
+#lines_printed_random(poem_lines)
