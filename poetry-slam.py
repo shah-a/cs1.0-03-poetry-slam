c2e18df7ffcabb384cc984e43e07161560d07e39
Ali Shah <s.ashah@outlook.com>
2020-09-29 09:35:01 -0400

    used equivalence_cycle() for lines_printed_custom()

--- poetry-slam.py
+++ poetry-slam.py
@@ -1,3 +1,6 @@
+#Ali Shah, github.com/shah-a
+#September 29th, 2020
+
 from random import randint
 
 def get_file_lines(filename):
@@ -40,35 +43,40 @@ def lines_printed_random(lines_list):
 
     return None
 
-# def equivalence_cycles(lines_list):
-#     new_list = []
-#     counter = 0
-#     while True:
-#         new_list.clear()
-#         counter += 1
-#         for _ in range(len(lines_list)):
-#             new_list.append(lines_list[randint(0, len(lines_list) - 1)])
+def equivalence_cycles(lines_list): #This function is for the lines_printed_custom() section of the assignment; I preferred to name it differently than instructed
 
-#         #print(new_list) #uncomment to print each list variation per cycle
-#         #input()         #uncomment to receive keyboard prompt to step through cycles; not recommended for lists with more than 3 elements
+    #Soo... I got curious and wrote this function to count how many cycles it takes for every element to randomly be assigned the same value
+    #It took 21530 cycles the first time I tried this with the poem from tanka.txt. I believe second and third attempts were around 64000 and 96000 cycles respectively
+    #All three attempts, each element ended up being 'A faint clap of thunder,' which makes sense because the poem has that line twice (lines 1 and 5)
+    #To maximize likelihood for most number of cycles, each list element should be unique
 
-#         if new_list[1:] == new_list[:-1]:
-#             break
+    new_list = []
+    counter = 0
+    while True:
+        new_list.clear()
+        counter += 1
+        for _ in range(len(lines_list)):
+            new_list.append(lines_list[randint(0, len(lines_list) - 1)])
 
-#     print(f"cycles: {counter}")
+        #print(new_list) #uncomment to print each list variation per cycle
+        #input()         #uncomment to receive keyboard prompt to step through cycles; not recommended for lists with more than 3 unique elements
 
-#     return None
+        if new_list[1:] == new_list[:-1]:
+            break
+        elif counter > 99999: #limits loop to 100,000 cycles; comment out to let program continue until all lines are equivalent
+            break
 
-#     #Soo... I got curious and wrote this function to count how many cycles it takes for every element to randomly be assigned the same value
-#     #It took 21530 cycles the first time I tried this with the poem from tanka.txt. Second attempt was around 64000, third was around 96000
-#     #All three attempts, each element ended up being 'A faint clap of thunder,' which makes sense because the poem has that line twice (lines 1 and 5)
-#     #To maximize likelihood for most number of cycles, each list element should be unique
+    print(f"Cycles: {counter}")
 
-def lines_printed_custom(lines_list):
-    pass
+    for line in new_list:
+        print(line[::-1])
+
+    return None
 
 poem_lines = get_file_lines("tanka.txt")
 
 #lines_printed_backwards(poem_lines)
 #lines_printed_random(poem_lines)
-#lines_printed_custom(poem_lines)
+equivalence_cycles(poem_lines)
+
+#equivalence_cycles(["one", "two", "three"])
