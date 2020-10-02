9f3b6389444df1c65b4028ea92acac4f8ba11b2c
Ali Shah <s.ashah@outlook.com>
2020-10-02 15:23:11 -0400

    update

--- poetry-slam.py
+++ poetry-slam.py
@@ -2,41 +2,41 @@
 # Ali Shah | Sept. 29, 2020
 # CS1.0 Assignment 3: Poetry Slam
 
-""" This program ... """
+"""This program reads poem lines from a file and manipulates them in various ways"""
 
 from random import randint
 
+
 def get_file_lines(filename):
+    """Reads poem file, removes newline char, and returns lines in a list"""
 
-    with open(filename, "r") as poem_lines:
-        poem_lines = poem_lines.readlines() #saves list of each poem lines in file_lines
+    with open(filename, "r") as lines:
+        lines = lines.readlines()  # Saves list of each poem lines in file_lines
 
-    for _ in range(len(poem_lines)):
-        poem_lines[_] = poem_lines[_].rstrip() #removes newline char from right-side end of each poem line
+    for _ in range(len(lines)):
+        lines[_] = lines[_].rstrip()  # Removes newline char from right-side end of each poem line
 
-    return poem_lines
+    return lines
 
 def lines_printed_backwards(lines_list):
+    """Adds numbers to poem's lines and reverses line order; prints result"""
 
+    numbered_poem = []
     for _ in range(len(lines_list)):
-        lines_list[_] = f"{_ + 1} {lines_list[_]}" #adds line number to beginning of each string in list
+        numbered_poem.append(f"{_ + 1} {lines_list[_]}")  # Adds line number to each string
 
-    lines_list.reverse()
+    numbered_poem.reverse()  # Reassigns all numbered_poem elements in reverse order
 
-    for _ in lines_list:
+    for _ in numbered_poem:
         print(_)
 
-    #  for _ in range(len(lines_list)):
-    #     print(lines_list[::-1][_])
-    #
-    # #Alternate solution to print the list in reverse order
-    # #Previous solution uses list reverse() method, which reassigns to the variable
-    # #This solution uses slicing with a negative step; it does not reassign any values to the list
-    # #Though this one is super neat :), I chose the previous solution in favour of code readability
-
-    return None
+    '''Alternate solution without reassignment to numbered_poem:
+    for _ in range(len(numbered_poem)):
+        print(numbered_poem[::-1][_])  # [::-1] -> negative step, start at last index
+    '''
 
 def lines_printed_random(lines_list):
+    """Prints random lines from poem until reaching original line count number of prints"""
 
     for _ in range(len(lines_list)):
         i = len(lines_list) - 1
@@ -44,29 +44,29 @@ def lines_printed_random(lines_list):
             lines_list[randint(0, i)]
         )
 
-    return None
-
-def equivalence_cycles(lines_list): #This function is for the lines_printed_custom() section of the assignment; I preferred to name it differently than instructed
-
-    #Soo... I got curious and wrote this function to count how many cycles it takes for every element to randomly be assigned the same value
-    #It took 21530 cycles the first time I tried this with the poem from tanka.txt. I believe second and third attempts were around 64000 and 96000 cycles respectively
-    #All three attempts, each element ended up being 'A faint clap of thunder,' which makes sense because the poem has that line twice (lines 1 and 5)
-    #To maximize likelihood for most number of cycles, each list element should be unique
+def equivalence_cycles(lines_list):  # lines_printed_custom() section of assignment
+    """
+    Randomizes poem's lines until every line is equivalent, then prints each line backwards
+    Stops at max_cycles attempts and prints the last attempt
+    To maximize likelihood for most number of cycles, each line in poem should be unique
+    """
 
     new_list = []
     counter = 0
+    max_cycles = 99999  # Used to limit attempts; recommended for lists with len >5
+
     while True:
         new_list.clear()
         counter += 1
         for _ in range(len(lines_list)):
             new_list.append(lines_list[randint(0, len(lines_list) - 1)])
 
-        #print(new_list) #uncomment to print each list variation per cycle
-        #input()         #uncomment to receive keyboard prompt to step through cycles; not recommended for lists with more than 3 unique elements
+        #print(new_list)  # Uncomment to print each list variation per cycle
+        #input()          # Uncomment for keyboard prompt to step cycle-by-cycle
 
         if new_list[1:] == new_list[:-1]:
             break
-        elif counter > 99999: #limits loop to 100,000 cycles; comment out to let program continue until all lines are equivalent
+        elif counter > max_cycles - 1:  # Limits loop to max_cycles attempts
             break
 
     print(f"Cycles: {counter}")
@@ -74,12 +74,9 @@ def equivalence_cycles(lines_list): #This function is for the lines_printed_cust
     for line in new_list:
         print(line[::-1])
 
-    return None
-
 poem_lines = get_file_lines("tanka.txt")
 
 #lines_printed_backwards(poem_lines)
 #lines_printed_random(poem_lines)
-equivalence_cycles(poem_lines)
-
-#equivalence_cycles(["one", "two", "three"])
+#equivalence_cycles(poem_lines)
+#equivalence_cycles(["one", "two", "three", "four", "five", "six"])
